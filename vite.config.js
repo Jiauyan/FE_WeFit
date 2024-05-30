dd51f956b24c0955ae11e455d5e616da662cfc59
Jiauyan <yanyantan0816@gmail.com>
2024-05-31 02:17:28 +0800

    config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@mui/icons-material']
+  },
   build: {
     rollupOptions: {
       external: ['@mui/icons-material']
     }
+  },
+  resolve: {
+    alias: {
+      '@mui/icons-material': '@mui/icons-material/esm'
+    }
   }
-})
+});
