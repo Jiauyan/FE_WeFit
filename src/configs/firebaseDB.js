1178d548c8f143af6adc4394a5d6bb9f358f9441
Jiauyan <yanyantan0816@gmail.com>
2024-11-13 00:45:02 +0800

    update

--- src/configs/firebaseDB.js
+++ src/configs/firebaseDB.js
@@ -1,38 +1,28 @@
-import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
 
+// Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-const storage = getStorage(app);
-const database = getDatabase(app);
 
-const connectfirebaseDB = async () => {
-  try {
-    console.log("Firebase connected")
-    return db;
-  } catch (err) {
-    console.error("Firebase Connection Error", err.message);
-  }
-};
+// Export Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const database = getDatabase(app);
 
-export {
-  connectfirebaseDB,
-  auth,
-  db,
-  storage,
-  database
-};
+export default app;
