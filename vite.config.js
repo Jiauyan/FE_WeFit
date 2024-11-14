67d0c708c437fbcc0a5fa6d74d709d82724ba9f8
Jiauyan <yanyantan0816@gmail.com>
2024-11-14 17:12:05 +0800

    update build to dist

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist',
     rollupOptions: {
       output: {
         manualChunks(id) {
