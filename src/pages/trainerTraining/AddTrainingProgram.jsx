112ccc97038ecaa04a89a911d17841bc71ba75eb
Jiauyan <yanyantan0816@gmail.com>
2024-11-19 17:10:49 +0800

    useEffect

--- src/pages/trainerTraining/AddTrainingProgram.jsx
+++ src/pages/trainerTraining/AddTrainingProgram.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import axios from 'axios';
 import { useUser } from "../../contexts/UseContext";
 import { useNavigate, Outlet } from 'react-router-dom';
