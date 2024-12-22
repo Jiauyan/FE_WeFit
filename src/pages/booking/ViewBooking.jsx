44195d88d809c9028feeabb13556036a06d58e10
Jiauyan <yanyantan0816@gmail.com>
2024-12-22 16:37:55 +0800

    cancel booking

--- src/pages/booking/ViewBooking.jsx
+++ src/pages/booking/ViewBooking.jsx
@@ -103,6 +103,9 @@ const canCancelBooking = () => {
     console.log("Cancellation window closed condition hit");
     return { canCancel: false, message: 'Cancellation has closed' }; // Within 3 days of the slot date
   }
+  if (bookingData.status == true){
+    return { canCancel: true, message: '' }; 
+  }
   console.log("Eligible for cancellation");
   return { canCancel: true, message: '' }; // Eligible for cancellation
 };
